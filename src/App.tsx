6892d9da719ae3619c135ced334ea7c1f909ad99
RYUSEULGI <71699667+RYUSEULGI@users.noreply.github.com>
2022-02-26 16:56:58 +0900

    feat: emotion reset 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React, { Suspense } from 'react';
-
 import { Route, Routes } from 'react-router';
 import SignupContainer from 'components/signup/SignupContainer';
+import { Global } from '@emotion/react';
+import { GlobalStyles } from 'styles/GlobalStyles';
 
 const App = () => {
   return (
     <>
+      <Global styles={GlobalStyles} />
       <Suspense fallback={<div> LOADING......</div>}>
         <Routes>
-          <Route path={'user/signup'} element={<SignupContainer />} />
+          <Route path={'/user/signup'} element={<SignupContainer />} />
         </Routes>
       </Suspense>
     </>
