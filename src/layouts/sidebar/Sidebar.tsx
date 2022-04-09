14ff42a597d7e8976c7a44c54d8ad12c9728b1b5
cksdnr3 <cksdnr3333@gmail.com>
2022-04-09 13:39:31 +0900

    feat: re-layout

---
+++ src/layouts/sidebar/Sidebar.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const Sidebar = () => {
+  return <Container></Container>;
+};
+
+const Container = styled.div`
+  width: 200px;
+  height: 100vh;
+  margin: 0px;
+  background-color: #c9c9c9;
+`;
+
+export default Sidebar;
