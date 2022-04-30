3ab384ae4e9d392cd03bb5ed82c5f0f7bcdec3f8
RYUSEULGI <71699667+RYUSEULGI@users.noreply.github.com>
2022-04-30 16:49:07 +0900

    feat: modal에 close 버튼 컴포넌트로 분리

---
+++ src/components/common/button/CloseButton.tsx
@@ -0,0 +1,38 @@
+import styled from '@emotion/styled';
+import React, { forwardRef } from 'react';
+import { IoClose } from 'react-icons/io5';
+
+interface Props {
+  backgroudColor?: string;
+  onClose?: () => void;
+}
+
+const CloseButton = forwardRef((props: Props, ref: any) => {
+  const buttonColor = props.backgroudColor === '#fff' ? '#000' : '#fff';
+  return (
+    <Button ref={ref} backgroudColor={props.backgroudColor}>
+      <span onClick={props.onClose}>
+        <IoClose color={buttonColor} fontSize={27} />
+      </span>
+    </Button>
+  );
+});
+
+export default CloseButton;
+
+const Button = styled.div<{ backgroudColor?: string }>`
+  display: flex;
+  flex-direction: row-reverse;
+  align-items: center;
+  padding: 8px 15px;
+  background-color: ${(props) => props.backgroudColor};
+
+  & > span {
+    transition: transform 0.17s;
+    cursor: pointer;
+  }
+
+  & > span:hover {
+    transform: rotate(-45deg);
+  }
+`;
