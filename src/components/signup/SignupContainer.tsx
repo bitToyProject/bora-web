a7151b924aa2cf1177eae429e764cf55c9ac1af0
Jinwook <wlsdnr4675@gmail.com>
2022-06-18 16:14:08 +0900

    feat: signup background

--- src/components/signup/SignupContainer.tsx
+++ src/components/signup/SignupContainer.tsx
@@ -10,7 +10,6 @@ const SignupContainer = () => {
 
   return (
     <SignupWrapper>
-      <h2>회원 가입</h2>
       <SignupPage />
     </SignupWrapper>
   );
@@ -19,35 +18,9 @@ const SignupContainer = () => {
 export default SignupContainer;
 
 const SignupWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-
-  flex: 1 1 1;
-  padding: 2rem;
-  border: 0.1rem solid black;
-  border-radius: 0.5rem;
-  h2 {
-    text-align: center;
-    margin-bottom: 1rem;
-    font-weight: bold;
-  }
-  label {
-    font-weight: bold;
-    margin: 0.5rem 0;
-  }
-  input {
-    height: 48px;
-    padding: 10px;
-    border: 1px solid #e0e3e6;
-    border-radius: 4px;
-    @media (min-width: 800px) {
-      max-width: 100%;
-    }
-  }
-  button {
-    justify-content: center;
-    display: flex;
-    align-items: center;
-    margin-top: 0.5rem;
-  }
+  position: relative;
+  height: 100vh;
+  background-image: url('/assets/signup_background.jpeg');
+  background-repeat: no-repeat;
+  background-size: cover;
 `;
--- src/components/signup/SignupPage.tsx
+++ src/components/signup/SignupPage.tsx
@@ -1,12 +1,12 @@
 import styled from '@emotion/styled';
 import { authAPI } from 'apis/auth';
 import axios from 'axios';
-import Button from 'components/common/Button';
-import Input from 'components/common/Input';
+import { TextButton } from 'components/common/button/TextButton';
+import { DraftComponent } from 'draft-js';
 import React, { ChangeEvent, MouseEvent, useState } from 'react';
-import { IMaskInput } from 'react-imask';
 import { useMutation } from 'react-query';
 import { useNavigate } from 'react-router';
+import Typist from 'react-typist';
 import { ISignupRequest } from 'types/auth.types';
 import { validation } from 'utils/validation';
 
@@ -16,6 +16,7 @@ interface Props {}
 
 const SignupPage = ({ ...props }: Props) => {
   const navigate = useNavigate();
+  const [step, setStep] = useState<number>(0);
   const [signupVal, setSignupVal] = useState<ISignupRequest>({
     authority: 0,
     firstName: '',
@@ -66,82 +67,105 @@ const SignupPage = ({ ...props }: Props) => {
   };
 
   return (
-    <>
-      <label>이메일</label>
-      <Input
-        type="email"
-        name="username"
-        value={signupVal.username}
-        onChange={(e) => handleInputChange(e)}
-      />
+    <SignupBox>
+      <NextBtnBox>
+        {/* <Typist>
+          <p> First Sentence </p>
+          <Typist.Delay ms={20} />
+          This won't be animated until 20ms after the first sentenced is rendered
+        </Typist> */}
+        <TextButton>123</TextButton>
+      </NextBtnBox>
+    </SignupBox>
+    // <>
+    //   <label>이메일</label>
+    //   <Input
+    //     type="email"
+    //     name="username"
+    //     value={signupVal.username}
+    //     onChange={(e) => handleInputChange(e)}
+    //   />
 
-      <label>비밀번호</label>
-      <Input
-        type="password"
-        name="password"
-        value={signupVal.password}
-        onChange={(e) => handleInputChange(e)}
-      />
+    //   <label>비밀번호</label>
+    //   <Input
+    //     type="password"
+    //     name="password"
+    //     value={signupVal.password}
+    //     onChange={(e) => handleInputChange(e)}
+    //   />
 
-      <label>비밀번호 확인</label>
-      <Input
-        type="password"
-        name="checkPassword"
-        value={signupVal.checkPassword}
-        onChange={(e) => handleInputChange(e)}
-      />
+    //   <label>비밀번호 확인</label>
+    //   <Input
+    //     type="password"
+    //     name="checkPassword"
+    //     value={signupVal.checkPassword}
+    //     onChange={(e) => handleInputChange(e)}
+    //   />
 
-      <label>성</label>
-      <Input
-        type="text"
-        name="firstName"
-        value={signupVal.firstName}
-        onChange={(e) => handleInputChange(e)}
-      />
+    //   <label>성</label>
+    //   <Input
+    //     type="text"
+    //     name="firstName"
+    //     value={signupVal.firstName}
+    //     onChange={(e) => handleInputChange(e)}
+    //   />
 
-      <label>이름</label>
-      <Input
-        type="text"
-        name="lastName"
-        value={signupVal.lastName}
-        onChange={(e) => handleInputChange(e)}
-      />
+    //   <label>이름</label>
+    //   <Input
+    //     type="text"
+    //     name="lastName"
+    //     value={signupVal.lastName}
+    //     onChange={(e) => handleInputChange(e)}
+    //   />
 
-      <label>전화번호</label>
-      <IMaskInput
-        type="tel"
-        mask={'000-0000-0000'}
-        value={signupVal.phoneNum}
-        name="phoneNum"
-        unmask={true}
-        onAccept={(value, mask) => {
-          // console.log(isString(value));
-          setSignupVal({ ...signupVal, phoneNum: value as string });
+    //   <label>전화번호</label>
+    //   <IMaskInput
+    //     type="tel"
+    //     mask={'000-0000-0000'}
+    //     value={signupVal.phoneNum}
+    //     name="phoneNum"
+    //     unmask={true}
+    //     onAccept={(value, mask) => {
+    //       // console.log(isString(value));
+    //       setSignupVal({ ...signupVal, phoneNum: value as string });
 
-          // if (isString(value)) {
-          //   setSignupVal({ ...signupVal, phoneNum: value });
-          // }
-        }}
-      />
-      <label>닉네임</label>
-      <Input
-        type="text"
-        name="nickName"
-        value={signupVal.nickName}
-        onChange={(e) => handleInputChange(e)}
-      />
-      <SingupBtnBox>
-        <Button text={'Move to Login'} onClick={() => navigate('/login')}></Button>
-        <Button text={'Play it with BORA'} onClick={(e) => reqSignup(e)}></Button>
-      </SingupBtnBox>
-    </>
+    //       // if (isString(value)) {
+    //       //   setSignupVal({ ...signupVal, phoneNum: value });
+    //       // }
+    //     }}
+    //   />
+    //   <label>닉네임</label>
+    //   <Input
+    //     type="text"
+    //     name="nickName"
+    //     value={signupVal.nickName}
+    //     onChange={(e) => handleInputChange(e)}
+    //   />
+    //   <SingupBtnBox>
+    //     <Button text={'Move to Login'} onClick={() => navigate('/login')}></Button>
+    //     <Button text={'Play it with BORA'} onClick={(e) => reqSignup(e)}></Button>
+    //   </SingupBtnBox>
+    // </>
   );
 };
 
 export default SignupPage;
 
-const SingupBtnBox = styled.div`
-  display: flex;
-  justify-content: center;
-  gap: 0.5rem;
+const SignupBox = styled.div`
+  position: absolute;
+  bottom: -4rem;
+  left: 50%;
+  right: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  width: 95%;
+  background-color: ${({ theme }) => theme.colors.background};
+  border: 0.5rem solid ${({ theme }) => theme.colors.background};
+  padding: 2rem;
+  color: ${({ theme }) => theme.colors.white};
+  height: 12rem;
+  border-radius: 0.5rem;
+  opacity: 0.8;
 `;
+
+const NextBtnBox = styled.div``;
